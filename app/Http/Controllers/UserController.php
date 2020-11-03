3f9dd45190e15cd4e46729fdb1b0929c2e7b5d2c
TibsGracia <54094227+TibsGracia@users.noreply.github.com>
2020-11-03 11:47:20 +0800

    Done for registration

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,7 @@ class UserController extends Controller
   public function authenticate(Request $request)
   {
     $credentials = $request->only('username', 'password');
-
+    $info = "";
     try {
       if (!$token = JWTAuth::attempt($credentials)) {
         return response()->json(['message' => 'invalid_credentials', 'status'=>400]);
@@ -23,14 +23,24 @@ class UserController extends Controller
       return response()->json(['message' => 'could_not_create_token', 'status'=> 500]);
     }
 
-    return response()->json(['status'=>200, 'token'=>$token, 'message'=> 'successfully_login']);
+    try{
+      $info = User::select("role")
+      ->where("username", "=", $request->get('username'))
+      ->get();
+    }catch(\Exception $e){
+      return response()->json(['message' => 'invalid_credentials', 'status'=> 400]);
+    }
+
+    return response()->json(['status'=>200, 'token'=>$token, 'message'=> 'successfully_login', "UserAccount"=>$info]);
   }
 
   public function register(Request $request)
   {
     $validator = Validator::make($request->all(), [
-      'username' => 'required',
-      'password' => 'required',
+      'uName' => 'required',
+      'phone' => 'required',
+      'pass' => 'required',
+       'role' => 'required',
     ]);
 
     if ($validator->fails()) {
@@ -38,8 +48,10 @@ class UserController extends Controller
     }
 
     $user = User::create([
-      'username' => $request->get('username'),
-      'password' => Hash::make($request->get('password')),
+      'username' => $request->get('uName'),
+      'phone' => $request->get('phone'),
+      'role' => $request->get('role'),
+      'password' => Hash::make($request->get('pass')),
     ]);
 
     $token = JWTAuth::fromUser($user);
