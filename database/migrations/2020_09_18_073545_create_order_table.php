2184f18e478cdac9050538858aabc9e060d5773b
TibsGracia <54094227+TibsGracia@users.noreply.github.com>
2020-11-06 15:46:20 +0800

    Route Guard

--- database/migrations/2020_09_18_073545_create_order_table.php
+++ database/migrations/2020_09_18_073545_create_order_table.php
@@ -15,16 +15,6 @@ class CreateOrderTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->bigIncrements('id');
-<<<<<<< HEAD
-            $table->string('customer_name');
-            $table->string('product_name');
-            // $table->string('customer_address');
-            // $table->string('contact_number');
-            $table->smallInteger('order_quantity');
-            $table->date('delivery_date');
-            // $table->double('longitude');
-            // $table->double('latitude');
-=======
             $table->integer('customer_id');
             $table->string('receiver_name');
             $table->bigInteger('contact_number');
@@ -32,7 +22,6 @@ class CreateOrderTable extends Migration
             $table->smallInteger('ubeHalayaJar_qty');
             $table->smallInteger('ubeHalayaTub_qty');
             $table->date('preferred_delivery_date');
->>>>>>> bad617497ff07be635a4bdd774d1b93f35d0ecd0
             $table->double('distance');
             $table->string('order_status');
             $table->timestamps();
