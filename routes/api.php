f7a33079bdeb3927c4c4d73731b01e438f010a95
Heavenjes <paulinecalatrava@gmail.com>
2020-11-12 16:56:20 +0800

    good

--- routes/api.php
+++ routes/api.php
@@ -33,7 +33,7 @@ Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'authenticate']);
 Route::get('open', [DataController::class, 'open']);
 
-// Route::group(['middleware' => ['jwt.verify']], function () {
+Route::group(['middleware' => ['jwt.verify']], function () {
     //Order Controller
     Route::post('/post/update', [OrderController::class, 'updateOrder']);
     Route::post('/post/createOrder', [OrderController::class, 'createOrder']);
@@ -131,4 +131,4 @@ Route::get('open', [DataController::class, 'open']);
 
     
  
-// });
+});
