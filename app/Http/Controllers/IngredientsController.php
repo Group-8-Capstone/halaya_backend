4ba0b27c6580f0b91315ce3f02d81b43880aa9b8
Heavenjes <paulinecalatrava@gmail.com>
2020-11-21 08:24:01 +0800

    new

--- app/Http/Controllers/IngredientsController.php
+++ app/Http/Controllers/IngredientsController.php
@@ -65,7 +65,7 @@ class IngredientsController extends Controller
         } catch(\Excetion $e){
             return response()->json($e);
         }
-        return 'sucess';
+        return 'success';
     }
 
     public function fetchUsedIng(){
